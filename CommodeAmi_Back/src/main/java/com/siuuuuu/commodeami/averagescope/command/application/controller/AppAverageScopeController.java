556c02417e31ec01a69900b8988cc8aba03c7129
siu98 <parancandy@naver.com>
2025-01-22 15:51:52 +0900

    feat: 별점 및 평균별점 연동

--- CommodeAmi_Back/src/main/java/com/siuuuuu/commodeami/averagescope/command/application/controller/AppAverageScopeController.java
+++ CommodeAmi_Back/src/main/java/com/siuuuuu/commodeami/averagescope/command/application/controller/AppAverageScopeController.java
@@ -1,25 +1,25 @@
-//package com.siuuuuu.commodeami.averagescope.command.application.controller;
-//
-//import com.siuuuuu.commodeami.averagescope.command.application.service.AppAverageScopeService;
-//import com.siuuuuu.commodeami.common.ResponseDTO;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/average-scope")
-//public class AppAverageScopeController {
-//
-//    private final AppAverageScopeService appAverageScopeService;
-//
-//    @Autowired
-//    public AppAverageScopeController(AppAverageScopeService appAverageScopeService) {
-//        this.appAverageScopeService = appAverageScopeService;
-//    }
-//    @PostMapping("")
-//    public ResponseDTO<?> saveAverageScope(@RequestParam("movieId") Long movieId,
-//                                           @RequestParam("scope") Double newScope,
-//                                           @RequestParam("oldScope") Double oldScope, // userId 대신 oldScope로 변경
-//                                           @RequestParam("isUpdate") boolean isUpdate) {
-//        return ResponseDTO.ok(appAverageScopeService.saveOrUpdateAverageScope(movieId, newScope, oldScope, isUpdate));
-//    }
-//}
+package com.siuuuuu.commodeami.averagescope.command.application.controller;
+
+import com.siuuuuu.commodeami.averagescope.command.application.service.AppAverageScopeService;
+import com.siuuuuu.commodeami.common.ResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/average-scope")
+public class AppAverageScopeController {
+
+    private final AppAverageScopeService appAverageScopeService;
+
+    @Autowired
+    public AppAverageScopeController(AppAverageScopeService appAverageScopeService) {
+        this.appAverageScopeService = appAverageScopeService;
+    }
+    @PostMapping("")
+    public ResponseDTO<?> saveAverageScope(@RequestParam("movieId") Long movieId,
+                                           @RequestParam("scope") Double newScope,
+                                           @RequestParam("oldScope") Double oldScope, // userId 대신 oldScope로 변경
+                                           @RequestParam("isUpdate") boolean isUpdate) {
+        return ResponseDTO.ok(appAverageScopeService.saveOrUpdateAverageScope(movieId, newScope, oldScope, isUpdate));
+    }
+}
--- CommodeAmi_Back/src/main/java/com/siuuuuu/commodeami/averagescope/command/application/service/AppAverageScopeService.java
+++ CommodeAmi_Back/src/main/java/com/siuuuuu/commodeami/averagescope/command/application/service/AppAverageScopeService.java
@@ -1,10 +1,10 @@
-//package com.siuuuuu.commodeami.averagescope.command.application.service;
-//
-//
-//import com.siuuuuu.commodeami.averagescope.command.aggregate.dto.AverageScopeDTO;
-//
-//public interface AppAverageScopeService {
-//
-//    AverageScopeDTO saveOrUpdateAverageScope(Long movieId, Double newScope, Double oldScope, boolean isUpdate); // userId 대신 oldScope 사용
-//
-//}
+package com.siuuuuu.commodeami.averagescope.command.application.service;
+
+
+import com.siuuuuu.commodeami.averagescope.command.aggregate.dto.AverageScopeDTO;
+
+public interface AppAverageScopeService {
+
+    AverageScopeDTO saveOrUpdateAverageScope(Long movieId, Double newScope, Double oldScope, boolean isUpdate); // userId 대신 oldScope 사용
+
+}
